d6a1490b39382e72b9ec44dda51cee4e712f6cc6
chiragmittal19 <chiragmittal19@gmail.com>
2017-11-04 22:39:52 +0530

    Update

--- app/src/main/java/com/csy/vquest/HomePageFragment.java
+++ app/src/main/java/com/csy/vquest/HomePageFragment.java
@@ -7,6 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
 
 public class HomePageFragment extends Fragment {
 
@@ -24,6 +32,51 @@ public class HomePageFragment extends Fragment {
         getActivity().setTitle("Home");
 
         View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        setHasOptionsMenu(true);
+
+        ListView listView = (ListView) view.findViewById(R.id.listViewHome);
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference questionRef = rootRef.child("question");
+
+        FirebaseListAdapter<QuestionBean> firebaseListAdapter = new FirebaseListAdapter<QuestionBean>(
+                getActivity(),
+                QuestionBean.class,
+                R.layout.question_display_layout,
+                questionRef
+        ) {
+            @Override
+            protected void populateView(View v, QuestionBean model, int position) {
+
+                TextView usernameView = (TextView) v.findViewById(R.id.usernameView1);
+                TextView timeView = (TextView) v.findViewById(R.id.timeView1);
+                TextView editedView = (TextView) v.findViewById(R.id.editedView1);
+                TextView qstringView = (TextView) v.findViewById(R.id.qstringView1);
+                TextView categoryView = (TextView) v.findViewById(R.id.categoryView1);
+                TextView viewsView = (TextView) v.findViewById(R.id.viewsView1);
+
+                if(model.getQanonymity() == 1)
+                    usernameView.setText("Anonymous");
+                else
+                    usernameView.setText(model.getUsername());
+
+                Date date = new Date(model.getTime());
+                timeView.setText(date.toString());
+
+                if(model.getQedited() == 1)
+                    editedView.setVisibility(View.VISIBLE);
+                else
+                    editedView.setVisibility(View.INVISIBLE);
+
+                qstringView.setText(model.getQstring());
+                categoryView.setText(model.getCategory());
+                viewsView.setText(String.valueOf(model.getViews()) + " views");
+
+            }
+        };
+
+        listView.setAdapter(firebaseListAdapter);
 
         floatBtn = (FloatingActionButton) view.findViewById(R.id.fab);
         floatBtn.setOnClickListener(new View.OnClickListener() {
