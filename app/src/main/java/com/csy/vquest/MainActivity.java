a34e0742a3fa09e83ac3cf336998777adfc2972f
Shubham <shubhamgupta.shiv@gmail.com>
2017-11-15 23:06:37 +0530

    Updated....

--- app/src/main/java/com/csy/vquest/MainActivity.java
+++ app/src/main/java/com/csy/vquest/MainActivity.java
@@ -6,17 +6,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
 
     private Button btn;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
-       Intent intent = new Intent(this, NavigationDrawerActivity.class);
+        Intent intent;
+
+        if (currentUser == null) {
+            intent = new Intent(this, SignInActivity.class);
+        }
+        else {
+            intent = new Intent(this, NavigationDrawerActivity.class);
+        }
+
         startActivity(intent);
 
     }
---
+++ app/src/main/java/com/csy/vquest/SignInActivity.java
@@ -0,0 +1,99 @@
+package com.csy.vquest;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.widget.Toast.makeText;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText emailText;
+    private EditText passText;
+    private Button loginBtn;
+    private TextView signupView;
+    private FirebaseAuth firebaseAuth;
+
+    private String email;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailText = (EditText) findViewById(R.id.input_email);
+        passText = (EditText) findViewById(R.id.input_password);
+        loginBtn = (Button) findViewById(R.id.btn_login);
+        signupView = (TextView) findViewById(R.id.link_signup);
+
+        loginBtn.setOnClickListener(this);
+        signupView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_login:
+                email = emailText.getText().toString().trim();
+                password = passText.getText().toString();
+
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                                    if(user.isEmailVerified()){
+                                        Intent intent = new Intent(SignInActivity.this, NavigationDrawerActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    else{
+                                        Toast.makeText(SignInActivity.this, "Verify your email by following the link sent to your registered email address.",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    makeText(SignInActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                                // ...
+                            }
+                        });
+                break;
+
+            case R.id.link_signup:
+                Intent intent = new Intent(this, SignUpActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+
+    }
+}
